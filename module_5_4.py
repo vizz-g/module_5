cdae363026218f2f775ab04ac779922b51c455ef
vizz-g <nikita355hleb@gmail.com>
2025-01-21 18:29:53 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,102 @@
+class House:
+    number_of_floors: object
+    houses_history = []
+
+    def __new__(cls, *args):
+        instance = super().__new__(cls)
+        instance.__init__(*args)
+        cls.houses_history.append(args[0])
+        cls.name = args[0]
+        cls.number_of_floors = args[1]
+        return instance
+
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor+1):
+                print(floor)
+
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        return f'Название: {self.name} кол-во этажей: {self.number_of_floors}'
+
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+
+    def __add__(self, value):
+        if not isinstance(value, int):
+            print("Значение должно быть целым числом")
+
+        self.number_of_floors += value
+        return self
+
+
+    def __iadd__(self, value):
+        if not isinstance(value, int):
+            print("Значение должно быть целым числом")
+
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        if not isinstance(value, int):
+            print("Значение должно быть целым числом")
+
+        self.number_of_floors += value
+        return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
